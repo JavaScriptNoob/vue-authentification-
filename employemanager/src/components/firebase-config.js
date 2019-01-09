2edf44479420704b4ab42f0208f052e519606f80
Timur Bobylev <timur.bobylev@yandex.ru>
2019-01-09 04:04:38 -0800

    its first commit, package.json has beeen added, build version

---
+++ employemanager/src/components/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBLlWNcjVQtmlAXbZJed4oYZvIrdoggSvU",
+    authDomain: "vuefb-f79ed.firebaseapp.com",
+    databaseURL: "https://vuefb-f79ed.firebaseio.com",
+    projectId: "vuefb-f79ed",
+    storageBucket: "vuefb-f79ed.appspot.com",
+    messagingSenderId: "69382994108"
+
+}
---
+++ employemanager/src/components/newEmployee.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="NewEmployee" >
+        <h3>New Employee</h3>
+    </div>
+</template>
+<script>
+export default {
+    name:'NewEmployee',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
